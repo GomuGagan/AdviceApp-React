59e7e1f180cdbfc50ad3f2bdb30ddb6a1f705a1f
GomuGagan <gagangomu@gmail.com>
2022-07-10 23:27:31 +0530

    fetched advice

--- src/App.js
+++ src/App.js
@@ -8,8 +8,20 @@ class App extends React.Component {
     state = {advice: ""};
 
     componentDidMount() {
+        this.fetchAdvice();
         console.log("compoent did mount");
-    }
+    };
+
+    fetchAdvice = () => {
+        axios.get("https://api.adviceslip.com/advice")
+        .then((response)=>{
+            const{ advice } = response.data.slip;
+            console.log(advice);
+        })
+        .catch((error)=>{
+            console.log("error");
+        })
+    };
     render() {
         return(
             <h1>app</h1>
